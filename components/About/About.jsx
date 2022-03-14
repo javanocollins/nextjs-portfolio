dd86c672425bbb0a76cf9f5208049d69825dfe5e
Javano Collins <jdcollins242@gmail.com>
2022-03-14 06:12:09 -0400

    update: added design

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
-const About = () => {
-  return (
-      <div>
-          <h1>About</h1>
-      </div>
-  );
-}
+const About = ({ devIcon, designIcon }) => {
+    const classes = `bg-white w-2/6 h-auto text-center flex flex-col items-center py-12 px-8`;
+    return (
+        <div className="flex justify-center divide-x-2">
+            <div className={`${classes}  rounded-l-3xl divi`}>
+                <FontAwesomeIcon icon={devIcon} className="text-red-600 w-14" />
+                <h2 className="pb-5 text-3xl font-bold">Developer</h2>
 
-export default About
+                <p className="w-full p-5 text-lg">
+                    I am a Frontend Developer that's always open to learning new
+                    technologies. I ensure that applications are beautiful,
+                    performant, and mobile responsive.
+                </p>
+
+                <p className="text-xl text-red-600 font-bold p-2">Tools</p>
+                <ul className="text-lg">
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>React Native</li>
+                    <li>Laravel</li>
+                    <li>Python</li>
+                    <li>MySQL</li>
+                    <li>Docker</li>
+                </ul>
+            </div>
+            <div className={`${classes} rounded-r-3xl`}>
+                <FontAwesomeIcon
+                    icon={designIcon}
+                    className="text-red-600 w-12"
+                />
+                <h2 className="pb-5 text-3xl font-bold">Designer</h2>
+                <p className="w-full p-5 text-lg">
+                    I have an extensive background in graphic design. I
+                    specialize in mobile and web design, logos, flyers, and
+                    business cards.
+                </p>
+
+                <p className="text-xl text-red-600 font-bold p-2">Tools</p>
+                <ul className="text-lg">
+                    <li>Figma</li>
+                    <li>Illustrator</li>
+                    <li>Photoshop</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default About;
