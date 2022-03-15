fc22537b7277fdc0a20eea48353b6475e088fcbe
Javano Collins <jdcollins242@gmail.com>
2022-03-15 06:22:27 -0400

    update: added size prop

--- components/Tool/Tool.jsx
+++ components/Tool/Tool.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 // Package imports
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
-const Tool = ({icon, title}) => {
+const Tool = ({icon, title, size}) => {
     return (
         <div className="text-center flex flex-col items-center justify-center relative">
             <FontAwesomeIcon
                 icon={icon}
                 title={title}
-                className="text-white text-8xl mb-4 w-24"
+                className={`${size} text-white text-8xl mb-4 -bottom-12 absolute`}
             />
-            {/* <p className="text-white absolute top-14">{title}</p> */}
+            <p className="text-white text-xl absolute top-14 uppercase">
+                {title}
+            </p>
         </div>
     );
 }
