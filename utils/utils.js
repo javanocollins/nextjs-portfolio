df9f74a654114c9f3e049d66d8d3ca8aed54e668
Javano Collins <jdcollins242@gmail.com>
2022-03-15 06:19:01 -0400

    update: added custom sizes for icons

--- utils/utils.js
+++ utils/utils.js
@@ -9,36 +9,43 @@ export const toolIcons = [
     {
         icon: ["fab", "react"],
         title: "React",
+        size: "w-28",
         activeColor: "",
     },
     {
         icon: ["fab", "js"],
         title: "JavaScript",
+        size: "w-24",
         activeColor: "",
     },
     {
         icon: faDocker,
+        size: "w-32",
         title: "Docker",
         activeColor: "",
     },
     {
         icon: faFigma,
         title: "Figma",
+        size: "w-20",
         activeColor: "",
     },
     {
         icon: ["fab", "python"],
         title: "Python",
+        size: "w-24",
         activeColor: "",
     },
     {
         icon: ["fab", "node"],
         title: "Node",
+        size: "w-28",
         activeColor: "",
     },
     {
         icon: faDatabase,
         title: "MySQL",
+        size: "w-20",
         activeColor: "",
     },
 ];
