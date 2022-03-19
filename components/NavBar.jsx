72cd2c1f9e91e71ea144226cdde083f4aee27265
Javano Collins <jdcollins242@gmail.com>
2022-03-19 06:47:52 -0400

    update: added logo to navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+
+import logo from "../images/logo.png";
 
 const NavBar = () => {
-  return (
-      <nav className="container mx-auto flex px-20 items-center pt-7 gap-8">
-          <Link href="/">Home</Link>
-          <Link href="/dev-work">Dev Work</Link>
-          <Link href="/design-work">Design Work</Link>
-      </nav>
-  );
-}
+    return (
+        <nav className="container mx-auto flex px-20 items-center pt-7 gap-8">
+            <Link href="/">
+                <Image
+                    className="cursor-pointer"
+                    src={logo}
+                    width={50}
+                    height={50}
+                />
+            </Link>
+            <Link href="/">Home</Link>
+            <Link href="/dev-work">Dev Work</Link>
+            <Link href="/design-work">Design Work</Link>
+        </nav>
+    );
+};
 
-export default NavBar
+export default NavBar;
