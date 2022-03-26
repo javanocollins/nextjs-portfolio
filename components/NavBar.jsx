35235c7f6b41c0ccfb7c6c4f0fb44ad6adae2c08
Javano Collins <jdcollins242@gmail.com>
2022-03-26 08:16:22 -0400

    fix: window dimensions

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -14,18 +14,18 @@ const NavBar = () => {
     const { height, width } = useWindowDimensions();
     const [viewMenu, setViewMenu] = useState(false);
 
+
     const showMenu = () => {
         setViewMenu((prevState) => {
             return !prevState;
         });
     };
 
-    const classes =
+    let classes =
         "bg-bgColor mx-auto flex lg:px-20 px-12 items-center justify-between pt-7 gap-8";
-    if (width > 768) {
-        classes =
-            "bg-bgColor flex flex-col lg:px-20 px-12 pt-7 justify-end items-end";
-    }
+
+    const mobileClasses =
+        "bg-bgColor flex flex-col lg:px-20 px-12 pt-7 justify-end items-end";
 
     return width > 768 ? (
         <nav className={classes}>
@@ -45,20 +45,20 @@ const NavBar = () => {
             </div>
         </nav>
     ) : (
-        <nav className={classes}>
+        <div className={mobileClasses}>
             <FontAwesomeIcon
                 icon={faBars}
                 className="text-white text-4xl mb-8 cursor-pointer hover:scale-90 py-2 pl-2 hover:transition-all duration-200	"
                 onClick={showMenu}
             />
             {viewMenu ? (
-                <div className="flex flex-col justify-end text-right gap-5 text-white text-lg">
+                <div className="flex flex-col text-right gap-5 text-white text-lg">
                     <Link href="/">Home</Link>
                     <Link href="/dev-work">Dev Work</Link>
                     <Link href="/design-work">Design Work</Link>
                 </div>
             ) : null}
-        </nav>
+        </div>
     );
 
     return null;
