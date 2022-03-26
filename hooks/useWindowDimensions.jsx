e7a20d0b4aff33fd4664a7fae69e48ecac350439
Javano Collins <jdcollins242@gmail.com>
2022-03-26 07:10:45 -0400

    update: added get windows dimensions

---
+++ hooks/useWindowDimensions.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            function handleResize() {
+                setWindowDimensions({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowDimensions;
+}
